eb45869ca99441486a1ec6e1edb13192ee5dc96e
basecampfukuoka <basecamp.fukuoka@gmail.com>
2025-11-27 20:16:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -187,8 +187,7 @@ details summary { cursor: pointer; font-weight:600; margin-bottom:4px; }
 """, unsafe_allow_html=True)
 
 # ---------- Filters UI ----------
-with st.sidebar:
-    st.header("フィルター / 検索")
+with st.expander("フィルター / 検索を表示", False):
     c1, c2, c3, c4, c5 = st.columns([0.2, 4, 0.5, 1,0.8])
 
     with c1:
