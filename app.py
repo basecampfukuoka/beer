7614240b405f8364c8a8b0605a64d6e75fd900ef
basecampfukuoka <basecamp.fukuoka@gmail.com>
2025-12-24 20:04:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -712,7 +712,7 @@ def render_beer_card(r, beer_id_safe, brewery):
             # ★ トグルボタン
             st.button(
             "詳細コメントを見る",
-                key=f"detail_comment_btn_{beer_id_safe}"
+                key=f"detail_comment_btn_{beer_id_safe}",
                 on_click=toggle_detail_comment,
                 args=(detail_comment_key,)
             )
